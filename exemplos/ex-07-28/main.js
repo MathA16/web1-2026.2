1c029495b6243e456c83f7ad9477ad0e9529cbb4
MathA16 <matheus.nascimento08@aluno.ifce.edu.br>
2026-08-03 21:40:56 -0300

    adiciona o exemplo trabalhado nas aulas de 28/07 a 03/08

---
+++ exemplos/ex-07-28/main.js
@@ -0,0 +1,22 @@
+console.log("Hello, World!");
+
+if (true) {
+    let teste = "hahahah";
+    console.log(teste);
+}
+
+
+let idade = 24;
+let nome = "Matheus Alves";
+
+console.log("minha idade é:" + idade);
+
+// seleciona um elemento pelo seu ID
+document.getElementById("pnome").innerHTML = nome + " tem: " + idade + " anos";
+
+const PI = 3.14;
+console.log(PI ** 2)
+
+let ultimo_nome; // Snake case
+let primeiroNome; // Camel case
+let NomeMeio; // pascal case
