39d6d2ed03047fdf233c6ec2876e85dccfc37c78
MathA16 <matheus.nascimento08@aluno.ifce.edu.br>
2026-08-04 21:39:02 -0300

    aula sobre operadores logicos basicos

---
+++ exemplos/ex-08-04/main.js
@@ -0,0 +1,16 @@
+let idade = 14; // number
+let altura = 1.5; // number
+
+let autorizado = idade >= 15 && altura > 1.4; // boolean
+
+if (autorizado) {
+    console.log("Entrada autorizada");
+} else {
+    console.log("Entrada não autorizada");
+}
+
+// operador ternário
+/* autorizado ? console.log("Entrada autorizada") : console.log("Entrada não autorizada");
+
+console.log(autorizado); */
+
