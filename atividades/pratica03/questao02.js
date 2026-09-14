fc989c3a59b89dbc49a6655256a9e75297c30820
MathA16 <matheus.nascimento08@aluno.ifce.edu.br>
2026-09-14 19:32:59 -0300

    atualizaçao q 2

--- atividades/pratica03/questao02.js
+++ atividades/pratica03/questao02.js
@@ -1,25 +1,63 @@
+
+// QUESTÃO 2 - BOLETIM DE NOTAS
+
+
+
+// Array de objetos com os DADOS DOS ALUNOS
+
 let alunos = [
-    { id: 1, nome: "João", idade: 20, },
-    { id: 2, nome: "Maria", idade: 22, },
-    { id: 3, nome: "Pedro", idade: 21, }
+    { nome: "Ana", notas: [8.5, 9] },
+    { nome: "Carlos", notas: [5, 6.7] },
+    { nome: "João", notas: [10, 8] },
+    { nome: "Maria", notas: [2.3, 3] }
 ];
 
-let alunosMedia = alunos.map(aluno => {
+
+// UM NOVO ARRAY COM NOME E MÉDIA
+// Usando MAP + REDUCE
+
+let alunosComMedia = alunos.map(aluno => {
+
+    // REDUCE: soma das duas notas do aluno
     let soma = aluno.notas.reduce((total, nota) => {
         return total + nota;
     }, 0);
-    let media = soma / aluno.notas.length;
-    return { nome: aluno.nome, media: media };
+
+    // Calculo média das 2 notas
+    let media = soma / 2;
+
+    //retorna novo array com o nome e a média do aluno
+    return {
+        nome: aluno.nome,
+        media: media
+    };
 });
 
-console.log("Alunos com média:");
-console.log(alunosMedia);
+
+// Mostra o resultado
+console.log("ALUNOS COM MÉDIA");
+console.table(alunosComMedia);
 
 
-let acimaDe7 = alunosMedia.filter(aluno => aluno.media > 7);
-console.log("Alunos com média acima de 7:");
-console.log(acimaDe7);
+// B) FILTRO DOS ALUNOS COM MÉDIA MAIOR QUE7
 
-let abaixoDe3 = alunosMedia.filter(aluno => aluno.media < 3);
-console.log("Alunos com média abaixo de 3:");
-console.log(abaixoDe3);
+let acimaDe7 = alunosComMedia.filter(aluno => {
+    return aluno.media > 7;
+});
+
+
+// Mostra os alunos media maior que 7
+console.log("ALUNOS COM MÉDIA MAIOR QUE 7");
+console.table(acimaDe7);
+
+
+// FILTRO DOS ALUNOS COM MÉDIA MENOR QUE 3
+
+let abaixoDe3 = alunosComMedia.filter(aluno => {
+    return aluno.media < 3;
+});
+
+
+// Mostra alunos media menor q 3
+console.log("ALUNOS COM MÉDIA MENOR QUE 3");
+console.table(abaixoDe3);
